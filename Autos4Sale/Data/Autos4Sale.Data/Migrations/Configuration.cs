92726620116a7adace0a011c00d42e708bb189a5
shopoff <shopov91@gmail.com>
2017-10-02 17:25:44 +0300

    update database made automatic when a new migration is added

--- Autos4Sale/Data/Autos4Sale.Data/Migrations/Configuration.cs
+++ Autos4Sale/Data/Autos4Sale.Data/Migrations/Configuration.cs
@@ -5,11 +5,12 @@ namespace Autos4Sale.Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Autos4Sale.Data.Auto4SaleDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<Autos4Sale.Data.Auto4SaleDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationDataLossAllowed = false;
         }
 
         protected override void Seed(Autos4Sale.Data.Auto4SaleDbContext context)
--- Autos4Sale/Web/Autos4Sale.Web/Global.asax.cs
+++ Autos4Sale/Web/Autos4Sale.Web/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autos4Sale.Data;
+using Autos4Sale.Data.Migrations;
 
 namespace Autos4Sale.Web
 {
@@ -12,6 +15,8 @@ namespace Autos4Sale.Web
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Auto4SaleDbContext, Configuration>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
