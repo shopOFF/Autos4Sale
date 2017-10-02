a6d6649cfe56a99a3813eea38756e095a26b8340
shopoff <shopov91@gmail.com>
2017-10-02 20:02:49 +0300

    added caroffer and image models

---
+++ Autos4Sale/Data/Autos4Sale.Data.Models/CarOffer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos4Sale.Data.Models
+{
+    public class CarOffer
+    {
+        private ICollection<Image> image;
+
+        public CarOffer()
+        {
+            this.image = new HashSet<Image>();
+        }
+
+        public int Id { get; set; }
+
+        public virtual User User { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DatePosted { get; set; }
+
+        public virtual ICollection<Image> Image
+        {
+            get { return this.image; }
+            set { image = value; }
+        }
+    }
+}
---
+++ Autos4Sale/Data/Autos4Sale.Data.Models/Image.cs
@@ -0,0 +1,13 @@
+﻿namespace Autos4Sale.Data.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual CarOffer CarOffer { get; set; }
+    }
+}
