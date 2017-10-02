e8bda49f69d250c61e67d8d46b83f0135d417db6
shopoff <shopov91@gmail.com>
2017-10-02 15:34:03 +0300

    abstracted user and dbcontext in to different projects

---
+++ Autos4Sale/Data/Autos4Sale.Data/Auto4SaleDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Autos4Sale.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos4Sale.Data
+{
+    public class Auto4SaleDbContext : IdentityDbContext<User>
+    {
+        public Auto4SaleDbContext()
+            : base("Autos4SaleDB", throwIfV1Schema: false)
+        {
+        }
+
+        public static Auto4SaleDbContext Create()
+        {
+            return new Auto4SaleDbContext();
+        }
+    }
+}
