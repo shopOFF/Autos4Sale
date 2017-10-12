dc60fbdeadcecff620f19705ee173ebb9dbef010
shopoff <shopov91@gmail.com>
2017-10-12 15:26:35 +0300

    added unit tests

---
+++ Autos4Sale/Tests/Autos4Sale.UnitTests/ControllersTests/OffersControllerTests/Constructor_Should.cs
@@ -0,0 +1,31 @@
+﻿using Autos4Sale.Services.Contracts;
+using Autos4Sale.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos4Sale.UnitTests.ControllersTests.OffersControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+
+        [TestCase]
+        public void OffersConstructor_WhenValidParametersArePased_ShouldNotBeNull()
+        {
+            // Arrange
+            var carOffersServiceMock = new Mock<ICarOffersService>();
+            var userServiceMock = new Mock<IUserService>();
+            //
+            // Act
+            OffersController offersController = new OffersController(carOffersServiceMock.Object, userServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(offersController);
+        }
+    }
+}
